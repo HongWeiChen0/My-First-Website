d256d4af332485f5ada3f4a65e3d384d8781314b
HongWeiChen <42477153+HongWeiChen0@users.noreply.github.com>
2018-09-03 18:55:53 -0400

    Added more buttons

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var button1El = document.getElementById("button1");
+var button2El = document.getElementById("button2");
+var button3EL = document.getElementById("button3");
+var button4El = document.getElementById("button4");
+var button5El = document.getElementById("button5");
 var paragraphsElements = document.getElementsByTagName("p");
 var listsElements = document.getElementsByTagName("li");
-var colorChange = function () {
+
+var colorChange = function() {
     var r = Math.floor(Math.random() * 255);
     var g = Math.floor(Math.random() * 255);
     var b = Math.floor(Math.random() * 255);
@@ -12,4 +17,30 @@ var colorChange = function () {
         listsElements[j].style.color = 'rgb(' + r + ', ' + g + ', ' + b + ')';
     }
 };
+
+var defaultColor = function() {
+    for (var i = 0; i < paragraphsElements.length; i++) {
+        paragraphsElements[i].style.color = 'rgb(' + 0 + ', ' + 0 + ', ' + 0 + ')';
+    }
+    for (var j = 0; j < listsElements.length; j++) {
+        listsElements[j].style.color = 'rgb(' + 0 + ', ' + 0 + ', ' + 0 + ')';
+    }
+};
+
+var bigFont = function() {
+    document.body.style.fontSize = "24px";
+};
+
+var smallFont = function() {
+    document.body.style.fontSize = "12px";
+};
+
+var normalFont = function() {
+    document.body.style.fontSize = "100%";
+}
+
 button1El.addEventListener("click", colorChange);
+button2El.addEventListener("click", defaultColor);
+button3EL.addEventListener("click", bigFont);
+button4El.addEventListener("click", smallFont);
+button5El.addEventListener("click", normalFont);
